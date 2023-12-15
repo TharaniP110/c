d9b4cd19add60c69701a810f2355dda42c890eb3
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2023-12-15 11:43:58 +0530

    prime number

---
+++ main (1).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int flag=0;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            flag++;
+        }
+    }
+    if(flag==0)
+    {
+        printf("Prime Number");
+    }
+    else
+    {
+        printf("Not a Prime Number");
+    }
+    
+}
