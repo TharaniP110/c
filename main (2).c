a071af52a3fe72a4392859861f3a0902db57a539
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2023-12-18 10:04:14 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int temp=n,count=0;
+    while(n>0)
+    {
+       n/=10;
+       count++;
+    }
+    n=temp;
+    int d,sum=0;
+    while(n>0)
+    {
+        d=n%10;
+        sum+=pow(d,count);
+        n/=10;
+    }
+    if(sum==temp)
+    {
+        printf("Armstrong number");
+    }
+    else
+    {
+        printf("Not an Armstrong number");
+    }
+}
