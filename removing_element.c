75f9bb9726063b22e089260332acb67980c0f959
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2023-12-20 11:13:07 +0530

    Create removing_element.c

---
+++ removing_element.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+
+{
+    int n,arr[50],a;
+    
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    
+    printf("Enter the position need to remove :");
+    scanf("%d",&a);
+    
+    printf("Enter array elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+    printf(" Reversed and after removing a element from array :");
+
+   for(int i=0;i<n;i++)
+    {
+        if(i==a){
+            continue;
+        }
+        else{
+            printf("%d ",arr[i]);
+        }
+    }
+}
