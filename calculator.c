8202d39d3ee8ecce80ae08884e167c04cd062fe2
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2023-12-21 10:33:30 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    
+    printf("Enter a:");
+    scanf("%d",&a);
+    
+    printf("Enter b:");
+    scanf("%d",&b);
+    
+    printf("%d",a+b);
+    printf("\n%d",a-b);
+    printf("\n%d",a*b);
+    printf("\n%d",a/b);
+    printf("\n%d",a%b);
+}
