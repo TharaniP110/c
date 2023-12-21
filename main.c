485159029f0b276ce4ab916d0ea36e8b5718cc4f
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2023-12-21 20:43:03 +0530

    ascending.c

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int arr[1000],n;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Enter elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
