9135c38276b25de60e92814310e03fba34495d27
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2023-12-24 07:00:34 +0530

    swap_case.c

---
+++ swap_case.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[10000];
+    scanf("%[^\n]s",str);
+  
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>='A' && str[i]<='Z')
+        {
+            str[i]+=32;
+        }
+        else if (str[i]>='a' && str[i]<='z')
+        {
+            str[i]-=32;
+        }
+        else
+        {
+            str[i]=str[i];
+        }
+    }
+  
+    printf("%s",str);
+}
