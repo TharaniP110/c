c1e1a93e469900784d972153ff99dca586611103
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2023-12-30 16:14:03 +0530

    find_max.c

---
+++ find_max.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int arr[1000],n;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Enter elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int max=0;
+    for(int i=0;i<n;i++)
+    {
+       if(arr[i]>max)
+       {
+           max=arr[i];
+       }
+    }
+    printf("The max element is : %d",max);
+}
