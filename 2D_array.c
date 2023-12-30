888dc85e3605a3970fc661ee69aece6ebbd900d3
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2023-12-31 00:22:19 +0530

    2D_array.c

---
+++ 2D_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() 
+
+{
+    int arr[1000][1000],row,col;
+  
+    printf("Enter number of elements:");
+    scanf("%d%d",&row,&col);
+  
+    printf("Enter elements:");
+    for(int i=0;i<row;i++)
+    {
+      for(int j=0;j<col;j++)
+      {
+        scanf("%d",&arr[i][j]);
+      }
+    }
+  
+    for(int i=0;i<row;i++)
+    {
+       for(int j=0;j<col;j++)
+       {
+           printf("%d",arr[i][j]);
+       }
+    }
+  
+}
