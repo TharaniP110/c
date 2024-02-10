907253e5b3a540272ac9d18f4d6a4b7b7220b94e
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2024-02-10 00:28:26 -0800

    fibonacci_series.c

---
+++ fibonacci_series.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,first=1,sec=1;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",first);
+        int nxt=first+sec;
+        first=sec;
+        sec=nxt;
+    }
+}
