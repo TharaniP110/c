5f4db5799e7d06f96e36233d2293585235ede3d7
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2024-02-13 07:44:54 -0800

    1st_and_last.c

---
+++ 1st_and_last.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    scanf("%s",str);
+    int len=strlen(str);
+    for(int i=0;i<len;i++)
+    {
+        if(i==0 || i==len-1)
+        {
+            printf("%c",str[i]);
+        }
+        else
+        {
+            printf("*");
+        }
+    }
+}
