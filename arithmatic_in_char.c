74765b627b215bf2d3743f935fba2f948744f7b3
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2024-03-05 05:17:09 -0800

    arithmatic_in_char.c

---
+++ arithmatic_in_char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   char a1='A',a2=1;
+   char add=' ',sub=' ',mul=' ',divi=' ';
+   add=a1+a2;
+   sub=a1-a2;
+   mul=a1*a2;
+   divi=a1/a2;
+   printf("ADD=%c ",add);
+   printf("SUB=%c ",sub);
+   printf("MULT=%c ",mul);
+   printf("DIVI=%c ",divi);
+}
