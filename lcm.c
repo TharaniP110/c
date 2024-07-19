0a1c0edfeaaab5249fa524bbce544893ed9e2df4
TharaniP110 <153631106+TharaniP110@users.noreply.github.com>
2024-07-19 18:38:02 +0530

    lcm.c

--- lcm.c
+++ lcm.c
@@ -5,7 +5,7 @@ int main()
     scanf("%d%d",&n1,&n2);
     int gcd,lcm;
     int smaller=(n1<n2)?n1:n2;
-    for(int i=1;i<smaller;i++)
+    for(int i=2;i<=smaller;i++)
     {
         if(n1%i==0 && n2%i==0)
         {
